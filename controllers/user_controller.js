1e8bf1493b652caa6b21cb7fef4fd8096ab387e2
gurinder <garrybains4u@gmail.com>
2023-02-02 01:06:22 -0500

    Twillio integration

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user_model.js';
 import { sendmail } from '../sendemail.js';
+import {sendText} from '../sms.js';
 
 
 export const createUser = async(req,res,next) =>{
@@ -16,8 +17,10 @@ export const updateUser = async(req,res,next) => {
     try {        
         const updatedUser = await User.update({first_name: req.body.first_name, middle_name: req.body.middle_name, last_name: req.body.last_name, email: req.body.email, user_name: req.body.user_name, mobile_number: req.body.mobile_number, address: req.body.address, dob: req.body.dob, gender: req.body.gender, blood_group: req.body.blood_group, role_id: req.body.rold_id},{where:{id: req.params.id}});
         const {email} = req.body;
+        const {mobilenum} = req.mobile_number;
         if(updatedUser > 0){
             sendmail(email);
+            sendText(mobilenum)
         }
         return res.status(200).json({"message":"User Updated"});
     } catch(e) {
