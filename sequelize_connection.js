e1b34133decba8dbcbe0021c134cd20f48ca1ef8
Pujan Gautam <mepujan10@gmail.com>
2023-02-02 09:20:53 -0500

    [resolved] merge conflict resolved

--- sequelize_connection.js
+++ sequelize_connection.js
@@ -5,5 +5,4 @@ import {config} from './configurations/config.js';
 export const sequelize = new Sequelize(config.db_name, config.db_user_name, config.db_password, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 8080
   });
