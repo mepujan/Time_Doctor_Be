affa666f4be749f8529213b2fc6028d84a71058f
Pujan Gautam <mepujan10@gmail.com>
2023-02-02 09:15:41 -0500

    [merge] sms notification feature

--- database_connection.js
+++ database_connection.js
@@ -4,7 +4,7 @@ import {config} from './configurations/config.js';
 const Connection = await mysql.createConnection({
     host: config.db_host_name,
     user: config.db_user_name,
-    password: config.db_password
+    password: config.db_password,
 });
 
 export default  Connection;
