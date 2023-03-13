f3e580410addb780fe5ab3946e6ecf56e0a62376
aniltimalsina <timalsinaanil96@gmail.com>
2023-03-13 17:51:47 -0400

    Added import backend

--- routes/calendarRoute.js
+++ routes/calendarRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getEvents,addEvents } from "../controllers/calendarController.js";
+import { getEvents,addEvents,updateEventFromFile } from "../controllers/calendarController.js";
 import { LoginRequired } from '../middlewares/login_required.js';
+import multer from 'multer';
+import { ProfilePictureStorage } from '../middlewares/image_handler.js';
 
 const calendarRoute = express.Router();
-
+const csv_upload = multer({storage:ProfilePictureStorage});
 calendarRoute.get("/api/events",LoginRequired,getEvents);
 calendarRoute.post("/api/setEvent",LoginRequired,addEvents);
+calendarRoute.post("/api/createFromCSV",csv_upload.single("test"),updateEventFromFile);
 
 export default calendarRoute;
