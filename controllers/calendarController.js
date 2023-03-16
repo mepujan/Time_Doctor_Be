0ae1498d55d21db3b5821806f9e42974e3858262
Pujan Gautam <mepujan10@gmail.com>
2023-03-15 23:33:09 -0400

    [added] color code

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -97,8 +97,8 @@ export const getEvents = async (req, res, next) => {
   return res.status(200).json(events);
 }
 
-const updateEvent = async(auth, newEvent) => {
-  let { start_date, end_date,eventId } = newEvent;
+const updateEvent = async (auth, newEvent) => {
+  let { start_date, end_date, eventId } = newEvent;
   start_date = new Date(start_date);
   end_date = new Date(end_date);
 
@@ -116,11 +116,11 @@ const updateEvent = async(auth, newEvent) => {
   try {
     const calendar = google.calendar({ version: 'v3', auth });
     const response = await calendar.events.get({
-      calendarId:'primary',
-      eventId:eventId
+      calendarId: 'primary',
+      eventId: eventId
     });
 
-    
+
     const formatted_time = start_date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     const html_patient = `<strong> Hi ${response.data.attendees[1].email.split('@')[0]} <br/>Your Surgery has been re-scheduled in Bluewater Health at  ${JSON.stringify(start_date).slice(1, 11)} at ${formatted_time}<br/>Regards,<br/>Time Doctor`;
     const html_doctor = `<strong> Hi Dr. ${response.data.attendees[0].email.split('@')[0]} <br/>Your Surgery has been re-scheduled in Bluewater Health at  ${JSON.stringify(start_date).slice(1, 11)} at ${formatted_time}<br/>Regards,<br/>Time Doctor`;
@@ -128,11 +128,11 @@ const updateEvent = async(auth, newEvent) => {
     calendar.events.patch({
       calendarId: 'primary',
       eventId: eventId,
-      requestBody:event
+      requestBody: event
     });
 
     sendmail(response.data.attendees[0].email, subject, html_doctor);
-    sendmail(response.data.attendees[1].email,subject,html_patient);
+    sendmail(response.data.attendees[1].email, subject, html_patient);
     return true;
   }
   catch (e) {
@@ -141,14 +141,16 @@ const updateEvent = async(auth, newEvent) => {
 }
 
 const addEvent = (auth, data, patient, doctor) => {
-  let { start_date, end_date } = data;
+  let { start_date, end_date, important } = data;
   start_date = new Date(start_date);
   end_date = new Date(end_date);
+
 
   const event = {
     'summary': 'Surgery Schedule',
     'location': '89 Norman St, Sarnia, ON N7T 6S3',
     'description': 'Your Surgery Has been Scheduled.',
+    'colorId': important ? 11 : 5,
     'start': {
       'dateTime': start_date,
       'timeZone': 'America/Toronto',
@@ -195,7 +197,7 @@ const addEvent = (auth, data, patient, doctor) => {
       return;
     }
     sendmail(patient.email, subject, html_patient);
-    sendmail(doctor.email,subject,html_doctor);
+    sendmail(doctor.email, subject, html_doctor);
     sendSMS([patient.mobile_number, doctor.mobile_number], message);
     return true
   });
@@ -215,13 +217,13 @@ export const addEvents = async (req, res, next) => {
 }
 
 
-export const rescheduleEvent = (req,res,next)=>{
+export const rescheduleEvent = (req, res, next) => {
   const newEvent = req.body;
-  const isUpdated = authorize().then((value)=>updateEvent(value,newEvent)).catch(console.error)
-  if(isUpdated){
-    return res.status(200).json({"message":"Schedule Updated Successfully"});
+  const isUpdated = authorize().then((value) => updateEvent(value, newEvent)).catch(console.error)
+  if (isUpdated) {
+    return res.status(200).json({ "message": "Schedule Updated Successfully" });
   }
-  return res.status(500).json({"message":"Cannot Update the Schedule"});
+  return res.status(500).json({ "message": "Cannot Update the Schedule" });
 }
 
 
@@ -230,19 +232,19 @@ export const rescheduleEvent = (req,res,next)=>{
 export const updateEventFromFile = async (req, res, next) => {
   try {
     csvParser.createReadStream(req.file.path)
-  .pipe(csv())
-  .on('data',async (row) => {
-    const { patient, doctor } = row;
-    const doctor_data = await User.findByPk(doctor, { attributes: ['email', 'mobile_number', 'user_name'] });
-    const patient_data = await User.findByPk(patient, { attributes: ['email', 'mobile_number', 'user_name'] });
-    authorize().then((value) => addEvent(value, row, patient_data, doctor_data)).catch(console.error);
-  })
-  .on('end', () => {
-    res.status(201).json({message: 'All events added successfully'});
-  });
+      .pipe(csv())
+      .on('data', async (row) => {
+        const { patient, doctor } = row;
+        const doctor_data = await User.findByPk(doctor, { attributes: ['email', 'mobile_number', 'user_name'] });
+        const patient_data = await User.findByPk(patient, { attributes: ['email', 'mobile_number', 'user_name'] });
+        authorize().then((value) => addEvent(value, row, patient_data, doctor_data)).catch(console.error);
+      })
+      .on('end', () => {
+        res.status(201).json({ message: 'All events added successfully' });
+      });
   }
-  catch (err){
-      next(err);
+  catch (err) {
+    next(err);
   }
 }
 
